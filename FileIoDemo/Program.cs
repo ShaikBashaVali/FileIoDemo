f4380a29164dd8dc216b399844850bf71eef0c33
Shaik Basha Vali <bashavali.shaik@outlook.com>
2023-03-29 00:50:06 +0530

    File_IO
    File Operation and File System for Creation, Deletion and Changes.
    SerializationDemo & DeSerialization

---
+++ FileIoDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FileIoDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FileIoDemo!");
+            //FileDemo.FileExists();
+            //FileDemo.ReadAllLines();
+            //FileDemo.ReadAllText();
+            //FileDemo.FileCopy();
+            //FileDemo.DeleteFile();
+            FileDemo.WriteUsingStreamWriter();
+        }
+    }
+}
